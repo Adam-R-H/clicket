dfdcff1913b818735b0b4b4e48e0079f57d6acc6
Adam-R-H <adam.rifqy@student.umn.ac.id>
2025-10-08 00:43:28 +0700

    replaced railway domain

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const USERS_FILE = path.join(__dirname, 'users.json');
 const EVENTS_FILE = path.join(__dirname, 'events.json');
 // Serve static files (for register.html and others)
@@ -175,4 +175,7 @@ app.post('/users/delete', (req, res) => {
     res.json({ success: true });
 });
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 module.exports = app;
